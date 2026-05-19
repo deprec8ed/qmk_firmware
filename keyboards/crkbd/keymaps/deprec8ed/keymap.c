641597b9081359a0c66e0d2d05d851bc25be1eea
deprec8ed <dominoday222@gmail.com>
2026-05-19 21:31:55 +0200

    added fix for numpad on windows systems

--- keyboards/crkbd/keymaps/deprec8ed/keymap.c
+++ keyboards/crkbd/keymaps/deprec8ed/keymap.c
@@ -48,11 +48,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TRNS,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PAST,   KC_P4,   KC_P5,   KC_P6, KC_PPLS, KC_PEQL,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PAST,    KC_4,    KC_5,    KC_6, KC_PPLS,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PSLS,   KC_P1,   KC_P2,   KC_P3, KC_PMNS, KC_TRNS,
+      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_PSLS,    KC_1,    KC_2,    KC_3, KC_PMNS, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_PDOT , KC_P0
+                                          KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,  KC_DOT,  KC_0
                                       //`--------------------------'  `--------------------------'
   ),
 
